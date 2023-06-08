5b518f6da0054ac1a29a5da8f6b7fb91b5ad53ba
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-06-08 23:09:27 +0300

    update fetch

--- src/redux/weather/weatherApi.js
+++ src/redux/weather/weatherApi.js
@@ -5,7 +5,7 @@ const API_KEY = 'f38907efd393452bb97153500230706';
 export const fetchWeather = async city => {
   try {
     const { data } = await axios.get(
-      `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
+      `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
     );
     console.log(data);
     return data;
