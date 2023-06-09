c6086fbb88162ef11ed23e3799e72d1b41c959a7
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-06-09 15:02:47 +0300

    add phone styles

--- src/components/pages/Weather/WeatherCard/WeatherCard.js
+++ src/components/pages/Weather/WeatherCard/WeatherCard.js
@@ -1,48 +1,95 @@
 import { BsWind, BsFillCloudsFill } from 'react-icons/bs';
 import css from './WeatherCard.module.css';
 
-export const WeatherCard = ({ data, curr }) => {
-  console.log('curr', curr);
-  console.log('data', data);
+import MediaQuery from 'react-responsive';
 
-  console.log(data.country === 'Украина' || data.country === 'Ukraine');
+export const WeatherCard = ({ data, curr }) => {
+  //  const isDesktopOrLaptop = useMediaQuery({
+  //    query: '(min-width: 1224px)',
+  //  });
+  //  const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' });
+  //  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+  //  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
+  //  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' });
+
   return (
     <div>
-      <div className={css.location}>
-        <p className={css.city}>
-          City : <span className={css.city_span}>{data.name}</span>
-        </p>
+      <MediaQuery minWidth={769}>
+        <div className={css.location}>
+          <p className={css.city}>
+            City : <span className={css.city_span}>{data.name}</span>
+          </p>
 
-        <p className={css.date}>
-          Date : <span className={css.date_span}>{data.localtime}</span>
-        </p>
-      </div>
-      <div className={css.current}>
-        <div className={css.container_current}>
-          <p className={css.temp}> {curr.temp_c} &#8451;</p>
-          <p className={css.wind}>
-            {<BsWind className={css.wind_icon} />} Wind: {curr.wind_kph} kph
+          <p className={css.date}>
+            Date : <span className={css.date_span}>{data.localtime}</span>
           </p>
         </div>
-        <div className={css.container_current}>
-          <img
-            className={css.img}
-            src={curr.condition.icon}
-            alt={curr.condition.text}
-            width={50}
-          />
-          <p className={css.wind}>{curr.condition.text}</p>
-        </div>
+        <div className={css.current}>
+          <div className={css.container_current}>
+            <p className={css.temp}> {curr.temp_c} &#8451;</p>
+            <p className={css.wind}>
+              {<BsWind className={css.wind_icon} />} Wind: {curr.wind_kph} kph
+            </p>
+          </div>
+          <div className={css.container_current}>
+            <img
+              className={css.img}
+              src={curr.condition.icon}
+              alt={curr.condition.text}
+              width={50}
+            />
+            <p className={css.wind}>{curr.condition.text}</p>
+          </div>
 
-        <div className={css.container_current}>
-          <p className={css.temp}>Humidity: {curr.humidity} %</p>
-          <p className={css.wind}>
-            {' '}
-            {<BsFillCloudsFill className={css.cloud_icon} />} Cloud:{' '}
-            {curr.cloud} %{' '}
+          <div className={css.container_current}>
+            <p className={css.temp}>Humidity: {curr.humidity} %</p>
+            <p className={css.wind}>
+              {' '}
+              {<BsFillCloudsFill className={css.cloud_icon} />} Cloud:{' '}
+              {curr.cloud} %{' '}
+            </p>
+          </div>
+        </div>
+      </MediaQuery>
+
+      <MediaQuery maxWidth={768}>
+        <div className={css.location_tel}>
+          <p className={css.city_tel}>
+            City : <span className={css.city_span_tel}>{data.name}</span>
+          </p>
+
+          <p className={css.date_tel}>
+            Date : <span className={css.date_span_tel}>{data.localtime}</span>
           </p>
         </div>
-      </div>
+        <div className={css.current_tel}>
+          <div className={css.container_current_tel}>
+            <p className={css.temp_tel}> {curr.temp_c} &#8451;</p>
+            <p className={css.wind_tel}>
+              {<BsWind className={css.wind_icon_tel} />} Wind: {curr.wind_kph}{' '}
+              kph
+            </p>
+          </div>
+          <div className={css.container_current_tel_img}>
+            <img
+              className={css.img_tel}
+              src={curr.condition.icon}
+              alt={curr.condition.text}
+              width={50}
+            />
+            <p className={css.wind_tel}>{curr.condition.text}</p>
+          </div>
+
+          <div className={css.container_current_tel}>
+            <p className={css.temp_tel}>Humidity: {curr.humidity} %</p>
+            <p className={css.wind_tel}>
+              {' '}
+              {<BsFillCloudsFill className={css.cloud_icon_tel} />} Cloud:{' '}
+              {curr.cloud} %{' '}
+            </p>
+          </div>
+        </div>
+      </MediaQuery>
     </div>
   );
 };
--- src/components/pages/Weather/WeatherList/WeatherList.js
+++ src/components/pages/Weather/WeatherList/WeatherList.js
@@ -9,22 +9,42 @@ import {
 
 import css from './WeatherList.module.css';
 
+import MediaQuery from 'react-responsive';
+
 export const WeatherList = () => {
   const location = useSelector(getWeatherLocation);
   const current = useSelector(getWeatherCurrent);
 
   return (
-    <div className={css.container}>
-      <ul className={css.list}>
-        {location.map((locat, index) => {
-          const curr = current[index];
-          return (
-            <li className={css.item} key={nanoid()}>
-              <WeatherCard data={locat} curr={curr} />
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <>
+      <MediaQuery minWidth={769}>
+        <div className={css.container}>
+          <ul className={css.list}>
+            {location.map((locat, index) => {
+              const curr = current[index];
+              return (
+                <li className={css.item} key={nanoid()}>
+                  <WeatherCard data={locat} curr={curr} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </MediaQuery>
+      <MediaQuery maxWidth={768}>
+        <div className={css.container_tel}>
+          <ul className={css.list}>
+            {location.map((locat, index) => {
+              const curr = current[index];
+              return (
+                <li className={css.item} key={nanoid()}>
+                  <WeatherCard data={locat} curr={curr} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </MediaQuery>
+    </>
   );
 };
